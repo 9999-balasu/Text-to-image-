7b291d0ac125ba203c5d3f776d1a49908388705c
9999-balasu <138554962+9999-balasu@users.noreply.github.com>
2024-11-21 22:01:00 +0530

    change

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,6 +3,7 @@ import Header from '../components/Header'
 import Steps from '../components/Steps'
 import Description from '../components/Description'
 import Testimonials from '../components/Testimonials'
+import GenerateBtn from '../components/GenerateBtn'
 const Home = () => {
   return (
     <div>
@@ -10,6 +11,7 @@ const Home = () => {
      <Steps/>
      <Description/>
      <Testimonials/>
+     <GenerateBtn/>
     </div>
   )
 }
